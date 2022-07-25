d693b38a605a5219130a8b75f125b0e0173b3b51
Sreenivas0596 <105493259+Sreenivas0596@users.noreply.github.com>
2022-07-25 14:55:20 +0530

    Update and rename src/context/ThemeContext.js to src/RestaurantContext.js

---
+++ src/RestaurantContext.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RestaurantContext = React.createContext({
+  cartList: [],
+  removeAllCartItems: () => {},
+  addCartItem: () => {},
+  removeCartItem: () => {},
+  incrementCartItemQuantity: () => {},
+  decrementCartItemQuantity: () => {},
+})
+
+export default RestaurantContext
+
