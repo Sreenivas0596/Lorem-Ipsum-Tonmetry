42091a8110ebe94107957c8b7c142dbd507effc3
Sreenivas0596 <105493259+Sreenivas0596@users.noreply.github.com>
2022-09-19 19:58:19 +0530

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,71 +1,36 @@
-// Write your code here
-import {Link} from 'react-router-dom'
-import ThemeContext from '../../context/ThemeContext'
+import Slider from 'react-slick'
+
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
 import './index.css'
 
-const Navbar = () => (
-  <ThemeContext.Consumer>
-    {value => {
-      const {isDarkTheme, toggleTheme} = value
-      return (
-        <>
-          {!isDarkTheme ? (
-            <div className="light-container">
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/website-logo-light-theme-img.png"
-                alt="website logo"
-              />
-              <ul className="list-container">
-                <Link to="/" className="link">
-                  <li> Home </li>
-                </Link>
-                <Link to="/about" className="link">
-                  <li> About</li>
-                </Link>
-              </ul>
-              <button
-                type="button"
-                onClick={toggleTheme}
-                className="button"
-                testid="theme"
-              >
-                <img
-                  src="https://assets.ccbp.in/frontend/react-js/dark-theme-img.png"
-                  alt="theme"
-                />
-              </button>
-            </div>
-          ) : (
-            <div className="dark-container">
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/website-logo-dark-theme-img.png"
-                alt="website logo"
-              />
-              <ul className="list-container">
-                <Link to="/" className="link">
-                  <li> Home </li>
-                </Link>
-                <Link to="/about" n className="link">
-                  <li> About</li>
-                </Link>
-              </ul>
-              <button
-                type="button"
-                onClick={toggleTheme}
-                className="button"
-                testid="theme"
-              >
-                <img
-                  src="https://assets.ccbp.in/frontend/react-js/light-theme-img.png"
-                  alt="theme"
-                />
-              </button>
-            </div>
-          )}
-        </>
-      )
-    }}
-  </ThemeContext.Consumer>
-)
+const ReactSlider = () => {
+  const settings = {
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  }
 
-export default Navbar
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        <div className="logo-container">
+          <h3>LOGO </h3>
+        </div>
+        <div className="logo-container">
+          <h3>LOGO </h3>
+        </div>
+        <div className="logo-container">
+          <h3>LOGO </h3>
+        </div>
+        <div className="logo-container">
+          <h3>LOGO </h3>
+        </div>
+      </Slider>
+    </div>
+  )
+}
+
+export default ReactSlider
+
